c02eb49a8df8734b6cfa6e3d4e7782a1c7dd4d4a
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-07-04 14:51:33 +0530

    check palindrome

--- recursionInStrings.cpp
+++ recursionInStrings.cpp
@@ -1,8 +1,23 @@
-// recursion in strings
+// check palindrome
 #include<iostream>
+#include<string>
 using namespace std;
 
+bool isPalindrome(string str, int start, int end) {
+    // base case
+    if (start >= end) {
+        return true;
+    }
+    if (str[start] != str[end]) {
+        return false;
+    }
+
+    return isPalindrome(str, start + 1, end - 1);
+}
+
 int main() {
+    string str = "aba";
+    cout << isPalindrome(str, 0, str.size() - 1);
 
     return 0;
 }
